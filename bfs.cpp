5b9dbc010ed83df649aa7424c41ba19dcdfdd463
Shash100int <56729143+Shash100int@users.noreply.github.com>
2022-10-28 14:07:06 +0530

    Update bfs.cpp

--- bfs.cpp
+++ bfs.cpp
@@ -1,22 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// This class represents a directed graph using
-// adjacency list representation
+
 class Graph
 {
 	int V; // No. of vertices
 
-	// Pointer to an array containing adjacency
-	// lists
+	
 	vector<list<int>> adj;
 public:
 	Graph(int V); // Constructor
 
-	// function to add an edge to graph
+	
 	void addEdge(int v, int w);
 
-	// prints BFS traversal from a given source s
+	
 	void BFS(int s);
 };
 
